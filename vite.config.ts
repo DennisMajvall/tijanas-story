7bcdd5a5697d18aa9fd33ed68558da32eb4431e5
Dennis Majvall <dennis.majvall@prisjakt.nu>
2025-07-19 16:47:05 +0200

    base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/tijanas-story/',
   build: {
     outDir: 'docs',
   },
